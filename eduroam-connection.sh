8e0a4e3b288be36349ed15e9d21c50d4397a3a09
Greg <88981845+Gregz9@users.noreply.github.com>
2023-02-12 12:57:20 +0100

    Delete eduroam-connection.sh

--- eduroam-connection.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# sets up eduroam with username
-# use nmcli connection up eduroam --ask to give password afterwards
-
-username="grzegork@uio.no"
-card="wlp1s0"
-nmcli connection add type wifi con-name "eduroam" ifname "$card" ssid "eduroam" -- wifi-sec.key-mgmt wpa-eap 802-1x.eap ttls 802-1x.phase2-auth mschapv2 802-1x.identity "$username"
-
