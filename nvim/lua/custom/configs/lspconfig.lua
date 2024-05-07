91fb66f106c9b0ae765b8d092b629e244a05cc7d
Gregz9 <gregorkajda@gmail.com>
2024-05-07 15:10:07 +0200

    Adding the custom catalog to the configuration
    Signed-off-by: Gregz9 <gregorkajda@gmail.com>

---
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,26 @@
+
+local config = require("plugins.configs.lspconfig")
+
+local on_attach = config.on_attach
+local capabilities = config.capabilities
+
+local lspconfig = require("lspconfig")
+
+-- if you just want default config for the servers then put them in a table
+local servers = { "html", "cssls", "tsserver", "clangd", "pyright" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"pyhton"},
+})
+
+-- 
+-- lspconfig.pyright.setup { blabla}
